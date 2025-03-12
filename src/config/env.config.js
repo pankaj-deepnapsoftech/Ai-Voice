2086421038edf6a73038342e6501154d23e48565
nitinthakurdev <radheradhe9870023@gmail.com>
2025-03-12 18:36:11 +0530

    some chnages

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -8,6 +8,9 @@ class Config {
     this.LOCAL_CLIENT_URL = process.env.LOCAL_CLIENT_URL;
     this.CLIENT_URL = process.env.CLIENT_URL;
     this.JWT_SECRET = process.env.JWT_SECRET;
+    this.EMAIL_ID = process.env.EMAIL_ID;
+    this.EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+
   }
 }
 
